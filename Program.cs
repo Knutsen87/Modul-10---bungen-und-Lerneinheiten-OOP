559b5104a2f3f77e73fc98f19dcc68c65e3f1653
Dennis Gillich <dennisgillich@live.de>
2022-03-03 21:47:54 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Hund
+{
+    public string Name { get; set; }
+    public byte Alter { get; set; }
+    public string Rasse { get; set; }
+    public string Geschlecht { get; set; }
+
+    static void Bellen(string Name)
+    {
+        Console.WriteLine( Name + "bellt!");
+    }
+    static void Fressen(string Name)
+    {
+        Console.WriteLine(Name + "frisst!");
+    }
+
+    public Hund(string name, byte alter, string rasse, string geschlecht)
+    {
+        Name = name;
+        Alter = alter;
+        Rasse = rasse;
+        Geschlecht = geschlecht;
+    }
+}
